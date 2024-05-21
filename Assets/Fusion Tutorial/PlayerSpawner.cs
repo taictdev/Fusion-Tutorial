6e5cd8a79107da95988a5a5be0730bcf89c2bb1e
Hieu Ngo <enginooby@gmail.com>
2024-05-21 13:46:34 +0700

    Update logic

---
+++ Assets/Fusion Tutorial/PlayerSpawner.cs
@@ -0,0 +1,16 @@
+using Fusion;
+using UnityEngine;
+
+public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
+{
+    public GameObject PlayerPrefab;
+    public GameObject CinemachineCameraTarget;
+
+    public void PlayerJoined(PlayerRef player)
+    {
+        if (player == Runner.LocalPlayer)
+        {
+           var obj = Runner.Spawn(PlayerPrefab, new Vector3(0, 1, 0), Quaternion.identity);
+        }
+    }
+}
